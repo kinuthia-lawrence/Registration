67b09ab60f40f50c02e3f1dec3296573c5ca5727
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-15 22:26:07 +0300

    created a cancelOnAction class

--- src/main/java/com/larrykin343/registration/LoginController.java
+++ src/main/java/com/larrykin343/registration/LoginController.java
@@ -1,8 +1,19 @@
 package com.larrykin343.registration;
 
+import com.almasb.fxgl.entity.action.Action;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+//this is a controller class for the login-view.fxml file
 public class LoginController {
 
+    @FXML
+    public Button cancelButton;//this is the cancel button
+
+    //this method is called when the cancel button is clicked
+    public  void cancelButtonOnAction(ActionEvent event) {
+        System.out.println("Cancel button clicked");
+    }
 }
